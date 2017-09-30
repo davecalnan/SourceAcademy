562395f2ce3eaab3287c2549319fabcdc20c0689
Dave Calnan <dave@jiff.ie>
2017-10-01 00:24:29 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+    html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+    }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .subtitle {
+        font-size: 28px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
+            @endauth
+        </div>
+        @endif
+
+        <div class="content">
+            <div class="title m-b-md">
+                SourceAcademy
+            </div>
+
+            <p class="subtitle m-b-md">
+                SourceAcademy is a platform that for hiring top quality student freelancers.
+            </p>
+
+            <p class="m-b-md">
+                Currently in private pilot programme. If you are interested in becoming a client or a freelancer please email below.
+            </p>
+
+            <div class="links">
+                <a href="https://davecalnan.me">By Dave Calnan</a>
+                <a href="mailto:d@ve.ie">Email me</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
