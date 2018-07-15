58e6b82cc2f58f91892af88b76c1e17f29f3ce23
Dave Calnan <d@ve.ie>
2018-07-16 03:40:35 +0700

    Update message

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::domain(env('APP_DOMAIN'))->group(function () {
     Route::post('subscriptions', 'SubscriptionController@store');
 
     Route::post('/done', function (\Illuminate\Http\Request $request) {
-        $message = "<@$request->user_id> completed $request->text";
+        $message = "✅ <@$request->user_id> completed $request->text";
 
         SlackLog::info($message);
         
