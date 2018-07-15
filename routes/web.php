1d29d56f681b101ebcd9e439303cb2fabc47b598
Dave Calnan <d@ve.ie>
2018-07-16 04:30:30 +0700

    Send Slack message manually

--- routes/web.php
+++ routes/web.php
@@ -43,11 +43,28 @@ Route::domain(env('APP_DOMAIN'))->group(function () {
     Route::post('subscriptions', 'SubscriptionController@store');
 
     Route::post('/done', function (\Illuminate\Http\Request $request) {
-        $message = "✅ <@$request->user_id> completed $request->text";
+        $message = (object) [
+            'text' => "✅ <@$request->user_id> completed:",
+            'attachments' => [
+                0 => (object) [
+                'color' => 'good',
+                'text' => $request->text,
+                ],
+            ],
+        ];
 
-        SlackLog::info($message);
+        $client = new \GuzzleHttp\Client();
+        $response = $client->post('https://hooks.slack.com/services/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', [
+            'body' => json_encode($message),
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+
+        $responseBody = $response->getBody()->getContents();
+        $statusCode = $response->getStatusCode();
         
-        return response($message, 200);
+        return response($responseBody, $statusCode);
     })->middleware(\Spatie\HttpLogger\Middlewares\HttpLogger::class);
 });
 
