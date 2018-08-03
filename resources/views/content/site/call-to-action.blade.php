2c79ea8279065ae1e71d6f1d8e404789655f1e84
Dave Calnan <d@ve.ie>
2018-08-03 17:42:08 +0800

    Remove start project link

--- resources/views/content/site/call-to-action.blade.php
+++ resources/views/content/site/call-to-action.blade.php
@@ -7,8 +7,9 @@
 					<h2 class="subtitle">All costs upfront, no hidden fees.</h2>
 				</div>
 				<div class="column is-4">
-					<a class="button is-black is-inverted" href="{{ route('signup') }}">Start a project</a>
-					<a onclick="Intercom('showNewMessage')" class="button is-black is-inverted is-outlined">Get in touch</a>
+					{{-- <a class="button is-black is-inverted" href="{{ route('signup') }}">Start a project</a> --}}
+					{{-- <a onclick="Intercom('showNewMessage')" class="button is-black is-inverted is-outlined">Get in touch</a> --}}
+					<a onclick="Intercom('showNewMessage')" class="button is-black is-inverted">Get in touch</a>
 				</div>
 			</div>
 		</div>
